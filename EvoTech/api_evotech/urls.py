449cd8706cc5376f5c17d4b77085e10aed5e1a3d
Soumeya <ks_mekki@esi.dz>
2023-06-01 11:06:42 +0100

    adding notifications interface

--- EvoTech/api_evotech/urls.py
+++ EvoTech/api_evotech/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from . import views
-from .views import Test, favorite, notification
+from .views import Test, favorite, notification , view_notifications
 
 
 urlpatterns = [
@@ -9,6 +9,7 @@ urlpatterns = [
     path('testing', views.Test, name='Test'),
     path('Preferable/<int:id_user>/<int:id_lieu>/', views.favorite, name='favorite'),
     path('Notification/<int:id_event>/', views.notification, name='notificaion'),
+    path('notifications/',view=view_notifications,name='all notifications')
 ]
 
 
