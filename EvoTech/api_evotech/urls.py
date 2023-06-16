965d9a3bc8a34f176526db5d3d7cfa238d7961d2
Soumeya <ks_mekki@esi.dz>
2023-06-16 11:26:11 +0100

    History done

--- EvoTech/api_evotech/urls.py
+++ EvoTech/api_evotech/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from . import views
-from .views import Test, favorite, notification , view_notifications, History_Ajout_Event, History_Modifier_Event,History_Supprimer_Event, History_Supprimer_Lieu, History_Modifier_Lieu,History_Ajout_Lieu
+from .views import Test, favorite, notification , view_notifications , History_Ajout_Event, History_Modifier_Event,History_Supprimer_Event, History_Supprimer_Lieu, History_Modifier_Lieu,History_Ajout_Lieu
 
 
 urlpatterns = [
     path('', views.index, name='index'),
     # path('/api', include('CBVapp.urls')),
     path('testing', views.Test, name='Test'),
+    path('Hist/',view=views.Hist,name="Hist"),
     path('Preferable/<int:id_user>/<int:id_lieu>/', views.favorite, name='favorite'),
     path('Notification/<int:id_event>/', views.notification, name='notificaion'),
     path('all_notifications/',view=views.view_notifications,name='all_notifications'),
